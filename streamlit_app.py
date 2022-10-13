454fc7178e5d516655540db3dfa0d80955c35dd5
Sammriddh Gupta <72302725+SammriddhGupta@users.noreply.github.com>
2022-10-13 18:48:56 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from snowflake import connector
 import streamlit as st
+from st_aggrid import AgGrid
 
 
 def get_snowflake_connector():
@@ -51,22 +52,9 @@ if __name__ == "__main__":
         unsafe_allow_html=True,
     )
     
-    card_template = """
-                <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-                    <div class="card bg-light mb-3" >
-                        <H5 class="card-header"> HelLO </h5>
-                            <div class="card-body">
-                                <span class="card-text"><b>Author(s): </b>{}</span><br/>
-                                <span class="card-text"><b>Year: </b>{}</span><br/>
-                                <span class="card-text"><b>Journal: </b>{}</span><br/>
-                                <span class="card-text"><b>DOI: </b>{}</span><br/>
-                                <span class="card-text"><b>Score: </b>{}</span><br/><br/>
-                                <p class="card-text">{}</p>
-                            </div>
-                        </div>
-                    </div>
-        """
 
+    info_df = pd.read_csv("../dataset/station_metadata.csv)
+    AgGrid(info_df)
     
 #Feature 1: station status and info----------------------------------------------------------------------------->
     # Get snowflake connector
@@ -80,9 +68,6 @@ if __name__ == "__main__":
     options = int(selected_station_description["STATION_ID"][0])
     
     all_station_info_df = pd.read_sql_query(f'SELECT * FROM citibike_status WHERE "id" = {options};', connector)
-    
-    for index, row in all_station_info_df.iterrows():
-        st.markdown(card_template.format(str(index + 1), row['station_id'], row['latitude'], row['longitude'], row['incoming_trips'], row['outgoing_trips'], row['all_trips'], row['kind']), unsafe_allow_html=True)
     
 
     left_col, right_col = st.columns(2)
